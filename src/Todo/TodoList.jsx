5559c5f50e5a3a2b304c000f08828a89b8b9c1a7
DuetoPark <inhye.park.dev@gmail.com>
2023-12-14 15:42:32 +0900

    ✨ LocalStorage 저장 구현

--- src/Todo/TodoList.jsx
+++ src/Todo/TodoList.jsx
@@ -5,29 +5,20 @@ import Task from './Component/Task';
 import todoReducer from '../Reducer/todoReducer';
 import AddForm from './Component/AddForm';
 import Wrapper from '../Layout/Wrapper';
-import { FcDataBackup, FcLandscape, FcNightLandscape } from "react-icons/fc";
+import { FcDataBackup } from "react-icons/fc";
 
 
 export default function TodoList() {
-  /**
-   * 어떤 스테이트가 있을까?
-   * 
-   * 다크/라이트
-   * 간편/보통
-   * 할일 리스트 - 내용, 완료여부
-   */
-
   const _taskType = [
     {type: 'all', text: '전체'},
     {type: 'yet', text: '해야할 일'},
-    {type: 'done', text: '완료한 일'}
+    {type: 'done', text: '완료한 일'},
   ];
 
-  const [_darkMode, setDarkMode] = useState(false);
-  const [_index, setIndex] = useState(1);
+  const [_index, setIndex] = useState(_indexInit);
   const [_filter, setFilter] = useState('all');
 
-  const [_toDoList, todoDispatch] = useReducer(todoReducer, []);
+  const [_toDoList, todoDispatch] = useReducer(todoReducer, _todoInit);
 
   const handleReset = () => {
     todoDispatch({type: 'reset'});
@@ -35,11 +26,13 @@ export default function TodoList() {
   }
 
   useEffect(() => {
-  }, []);
+    localStorage.setItem('index', _index);
+    localStorage.setItem('todo', JSON.stringify(_toDoList));
+  }, [_toDoList, _index]);
 
   return (
-    <Wrapper>
-      <section className={`todo`}>
+    <Wrapper extraStyle='todo-wrapper'>
+      <section className='todo'>
         <header className='todo-header'>
           <h1 className='todo-title'>멋쟁이들만 쓸 수 있는 오늘의 할 일</h1>
 
@@ -55,16 +48,7 @@ export default function TodoList() {
             </div>
             
             <div className='right-box'>
-              <label className='icon-btn btn-mode'>
-                <input
-                  className='visually-hidden'
-                  type="checkbox"
-                  onChange={(e) => {setDarkMode(prev => !prev)}}
-                />
-                {_darkMode ? <FcLandscape title='라이트 모드' /> : <FcNightLandscape title='다크 모드'/>}
-              </label>
-
-              <button className='icon-btn' type='button' onClick={handleReset}><FcDataBackup /></button>
+              <button className='' type='button' onClick={handleReset}><FcDataBackup /></button>
             </div>
           </div>
         </header>
@@ -77,7 +61,7 @@ export default function TodoList() {
                 if (_filter === 'done') return item.isDone;
                 return true
               })
-              .map(item => (
+              .map((item, index) => (
                 <li key={item.idx} className='task-item'>
                   <Task _todoData={item} todoDispatch={todoDispatch} />
                 </li>
@@ -98,5 +82,5 @@ export default function TodoList() {
   );
 }
 
-// const _indexInit = +window.localStorage.getItem('index');
-// const _todoInit = JSON.parse(window.localStorage.getItem('todo'));
+const _indexInit = +localStorage.getItem('index') || 1;
+const _todoInit = JSON.parse(window.localStorage.getItem('todo')) || [];
