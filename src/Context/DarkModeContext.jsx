3166bff55d7e03a14cad8d8ed3fa3306611afbdd
DuetoPark <inhye.park.dev@gmail.com>
2023-12-15 22:10:11 +0900

    🚧 Dark mode - Context에 로컬스토리지 저장, 클래스 추가, 마운트 될 때 초기값 설정 구현

--- src/Context/DarkModeContext.jsx
+++ src/Context/DarkModeContext.jsx
@@ -1,9 +1,22 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 const DarkModeContext = createContext();
 
 export default function DarkModeProvider({children}) {
   const [_darkMode, setDarkMode] = useState(false);
-  const toggleMode = () => setDarkMode(prev => !prev);
+  const toggleMode = () => {
+    // NOTE: state 수정
+    setDarkMode(!_darkMode);
+
+    // NOTE: 스타일 수정
+    updateDarkMode(!_darkMode);
+  };
+
+  useEffect(() => {
+    // console.log(('theme' in localStorage));
+    const isDark = localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
+    setDarkMode(isDark);
+    updateDarkMode(isDark);
+  }, []);
 
   return (
     <DarkModeContext.Provider value={{_darkMode, toggleMode}}>
@@ -11,5 +24,16 @@ export default function DarkModeProvider({children}) {
     </DarkModeContext.Provider>
   );
 }
+
+const updateDarkMode = (_darkMode) => {
+  if (_darkMode) {
+    document.documentElement.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+    localStorage.setItem('theme', '');
+  }
+
+};
 
 export const useDarkModeContext = () => useContext(DarkModeContext);
